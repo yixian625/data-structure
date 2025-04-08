3873bb36cd96544fe4e6b7b67af4787b57e9522d
Yixian Li <yixian625@gmail.com>
2025-04-08 09:08:30 -0400

    add input files

---
+++ lab3/lab3/utils/PriorityListClass.py
@@ -0,0 +1,97 @@
+# a priority class that help sort the nodes based on their priority,
+# sort the list using merge sort when new items are inserted into the list
+
+from lab3.utils.NodeClass import TreeNode
+
+
+class PriorityListClass:
+
+    def __init__(self):
+        self.nodes = []
+
+    def insert(self, node: TreeNode):
+        self.nodes.append(node)
+
+    def pop_first(self):
+        if len(self.nodes) == 0:
+            raise IndexError("There is no item left in the list to pop.")
+        item = self.nodes[0]
+        self.nodes = self.nodes[1:]
+        return item
+
+    def sort(self):
+        self.nodes = self._recursive_merge_sort(self.nodes)
+
+    def _recursive_merge_sort(self, node_list):
+        """
+        Recursively split the nodes in node_list, sort them,
+        then merge back to one sorted list. The nodes that should
+        be used first in the huffman tree is put that the beginning of
+        the list.
+        :param node_list: list of nodes to sort
+        :return: sorted list
+        """
+        # base case:
+        if len(node_list) == 1:
+            return node_list
+
+        # recursive calls:
+        middle = len(node_list)//2
+        left = self._recursive_merge_sort(node_list[:middle])
+        right = self._recursive_merge_sort(node_list[middle:])
+        return self._merge(left, right)
+
+    @staticmethod
+    def _merge(left, right):
+        sorted_list=[]
+        # start pointers
+        i = j = 0
+        # merge until one of the subfile is depleted
+        while i < len(left) and j < len(right):
+
+            if left[i].freq < right[i].freq:
+                sorted_list.append(left[i])
+                i += 1
+
+            # resolving ties
+            if left[i].freq == right[i].freq:
+                # prioritize the single-letter group when frequencies are the same
+                if len(left[i].item) == 1 and len(right[i].item) > 1:
+                    sorted_list.append(left[i])
+                    i += 1
+
+                elif len(right[i].item) == 1 and len(left[i].item) > 1:
+                    sorted_list.append(right[i])
+                    j += 1
+
+                # if both contain single or both contain multiple letters,
+                # use the smallest alphabet in the group to solve tie
+                else:
+                    smallest_left = min(left[i])
+                    smallest_right = min(right[i])
+                    if smallest_left < smallest_right:
+                        sorted_list.append(left[i])
+                        i += 1
+                    else:
+                        sorted_list.append(right[i])
+                        j += 1
+
+            else:
+                sorted_list.append(right[i])
+                j += 1
+
+        # when either left or right is depleted,
+        # add the rest of the other subfile
+        sorted_list.extend(left[i:])
+        sorted_list.extend(right[j:])
+
+        return sorted_list
+
+
+
+
+
+
+
+
+
