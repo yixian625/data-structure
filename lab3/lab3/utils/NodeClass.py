831fcba00319f39f5b161b9bfae9bcee95fadcab
Yixian Li <yixian625@gmail.com>
2025-04-08 09:06:59 -0400

    add priority list class to manage the list of nodes based on their frequencies/ items

---
+++ lab3/lab3/utils/NodeClass.py
@@ -0,0 +1,13 @@
+# This module contains the class for the Node instances of the binary tree,
+# implemented as a linked list
+
+
+class TreeNode:
+
+    def __init__(self, char, freq):
+        self.item = char
+        self.freq = freq
+        self.left_child = None
+        self.right_child = None
+
+
