6382c9b786cd3f21f42094aec7a44cf1fa8a4c82
Yixian Li <yixian625@gmail.com>
2025-04-26 09:07:40 -0400

    add natural merge sort and corresponding tests

--- lab4/lab4/MergeSortClass.py
+++ lab4/lab4/MergeSortClass.py
@@ -3,9 +3,15 @@ from lab4.NodeClass import Node
 
 class MergeSort:
 
-    def __init__(self, array):
+    def __init__(self):
         # turn the array containing data to an array of nodes with next pointers (initiated as None)
-        self.array = [Node(item) for item in array]
+        self.array = []
+
+    def __sizeof__(self):
+        return len(self.array)
+
+    def add_node(self, item):
+        self.array.append(Node(item))
 
     def merge_sort_basic(self):
         """
@@ -13,7 +19,16 @@ class MergeSort:
         to avoid taking extra space
         :return: the head node of the sorted list
         """
-        head = MergeSort._recursive_merge_sort_basic(self.array)
+        head = MergeSort._recursive_merge_sort_basic(self.array, 0, len(self.array)-1)
+        return head
+
+    def merge_sort_natural(self):
+        """
+
+        :return:
+        """
+        subfiles_pointers = MergeSort._get_natural_subfiles(self.array)
+        head = MergeSort._recursive_natural_merge_sort(self.array, subfiles_pointers)
         return head
 
     @staticmethod
@@ -26,7 +41,7 @@ class MergeSort:
         """
         # if there's only one subfile, return it directly
         if len(start_pointer_list) == 1:
-            return node_array[start_pointer_list]
+            return node_array[start_pointer_list[0]]
 
         # break the subfiles into groups of two recursively
         middle_start_index = len(start_pointer_list)//2
@@ -39,10 +54,25 @@ class MergeSort:
     @staticmethod
     def _get_natural_subfiles(node_array):
         """
-
         :param node_array:
         :return: a list of indices pointing to the start of each subfile in the array
         """
+
+        start_pointers = [0]
+
+        i = 0
+        while i < len(node_array)-1:
+            # advance the pointer until the item is smaller than the current item
+            while i < len(node_array)-1 and node_array[i].data <= node_array[i+1].data:
+                node_array[i].next = node_array[i+1] # set up the next pointers for nodes within each subfile
+                i += 1
+            # move pointer one move place to the right to start the new subfile
+            if i != len(node_array)-1:
+                i += 1
+                start_pointers.append(i)
+
+        return start_pointers
+
 
     @staticmethod
     def _recursive_merge_sort_basic(node_array, array_start, array_end):
--- lab4/tests/test_merge_sorts.py
+++ lab4/tests/test_merge_sorts.py
@@ -7,15 +7,38 @@ from lab4.MergeSortClass import MergeSort
 class MyTestCase(unittest.TestCase):
 
     def setUp(self):
-        items = list(range(100))
-        random.shuffle(items)
-        self.node_array = [Node(i) for i in items]
+        self.items = list(range(100))
+        random.shuffle(self.items)
 
     def test_recur_merge_basic(self):
 
-        array = self.node_array.copy()
+        array = [Node(i) for i in self.items]
 
         sorted_head = MergeSort._recursive_merge_sort_basic(array, 0, len(array)-1)
+
+        sorted_list = []
+        cur_node = sorted_head
+        while cur_node is not None:
+            sorted_list.append(cur_node.data)
+            cur_node = cur_node.next
+
+        self.assertEqual(sorted_list, list(range(100)))  # add assertion here
+
+    def test_natural_merge_subfile_creation(self):
+        items = [0, 1, 5, 2, 10, 9, 11, 7, 99, 20]
+        expected_pointers = [0, 3, 5, 7, 9]
+        node_array = [Node(i) for i in items]
+        returned_pointers = MergeSort._get_natural_subfiles(node_array)
+
+        self.assertEqual(expected_pointers, returned_pointers)
+
+    def test_natural_merge(self):
+
+        merge_array_instance = MergeSort()
+        for i in self.items:
+            merge_array_instance.add_node(i)
+
+        sorted_head = merge_array_instance.merge_sort_natural()
 
         sorted_list = []
         cur_node = sorted_head
